df9c1895018b1fcee81035b4baf68d351c2e41c2
Naza Napoli <nazanapoli@icloud.com>
2022-06-19 20:41:24 -0300

    FILTROS

--- javascript/productos.js
+++ javascript/productos.js
@@ -2,8 +2,8 @@ import Producto from './classVelas.js'
 const arrayStock = [
   new Producto(1,'Vela Apoel','500g','Coconut',670,'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/261/858/products/218fcfac-ceef-4def-8741-d89354205f28-2548678907693e45d716442702125362-640-0.jpeg'),
   new Producto(2,'Vela Burgio','350g','Lemon Grass',900,'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/261/858/products/b377a23a-e5b6-40cc-9f05-fb4ba9a34e1d-494f38c12054145f3116346029035665-640-0.jpeg'),
-  new Producto(3,'Vela Casteldefells','475g','Vainilla',1499,'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/261/858/products/8e7a69cb-8b9d-4cdf-8d2d-97ba44dcdfd3-012cc258c7d14151f316442582167641-1024-1024.jpeg'),
-  new Producto(4,'Vela Dinamo','270g','Chocolate',999,'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/261/858/products/fc271cb4-6163-418f-b87c-b52b0279d715_nube-764eb6126f8a181d3616065106131993-640-0.jpg'),
+  new Producto(3,'Vela Casteldefells','475g','Vainilla',1599,'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/261/858/products/8e7a69cb-8b9d-4cdf-8d2d-97ba44dcdfd3-012cc258c7d14151f316442582167641-1024-1024.jpeg'),
+  new Producto(4,'Vela Dinamo','270g','Chocolate',1099,'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/261/858/products/fc271cb4-6163-418f-b87c-b52b0279d715_nube-764eb6126f8a181d3616065106131993-640-0.jpg'),
 ];
 // Desestructuraciones
 const nombreProducto = (productoADesestructurar) => {
@@ -123,14 +123,15 @@ function cargaProductos(arFiltrado) {
     localStorage.removeItem('carritoValor');
   });
 }
-//Filtro aromas
+//creacion de filtro aromas en HTML
+let idAromas = 1 //genera ID dinamicos
 for (const vela of arrayStock) {
   let containerInput = document.createElement('div');
-  containerInput.innerHTML = `<div class="containerInput" id="containerInput"><input type="radio" name="my-input" id="no"><label for="no">${vela.aroma}</label></div>`;
+  containerInput.innerHTML = `<div class="containerInput" id="containerInput"><input type="radio" value="${vela.aroma}" name="filtro" id="aroma${idAromas++}"><label for="no">${vela.aroma}</label></div>`;
   divFiltroAroma.append(containerInput);
 }
 cargaProductos(arrayStock)
-
+// busqueda con input y lupa
 const filtroPorBusqueda = () => {
   const busquedaUsuario = busqueda.value.toUpperCase()
   const arFiltrado = arrayStock.filter((vela)=>{
@@ -142,3 +143,77 @@ lupa.addEventListener('click',filtroPorBusqueda)
 busqueda.addEventListener('keyup',()=>{
   (busqueda.value=='')?cargaProductos(arrayStock):filtroPorBusqueda()
 })
+//Filtro Por Precio
+let primerCheckboxPrecio = document.getElementById('menosDeMil')
+let segundoCheckboxPrecio = document.getElementById('EntreXeY')
+let tercerCheckboxPrecio = document.getElementById('desde')
+
+//1check
+primerCheckboxPrecio.addEventListener('change',primerCheckFiltro,false)
+function primerCheckFiltro (){
+const filtroPorPrecio = () => {
+  const precioSeleccionadoUsuario = primerCheckboxPrecio.value
+  const arFiltradoPrecio1 = arrayStock.filter((vela)=>{
+    return vela.precio <= precioSeleccionadoUsuario
+  });
+    cargaProductos(arFiltradoPrecio1)
+}
+filtroPorPrecio(arrayStock)
+}
+
+// 2check
+segundoCheckboxPrecio.addEventListener('change',segundoCheckboxFiltro,false)
+function segundoCheckboxFiltro (){
+const filtroPorPrecio = () => {
+  const precioSeleccionadoUsuario = segundoCheckboxPrecio.value
+  const arFiltradoPrecio2 = arrayStock.filter((vela)=>{
+    return (vela.precio > precioSeleccionadoUsuario && vela.precio<tercerCheckboxPrecio.value)
+  });
+    cargaProductos(arFiltradoPrecio2)
+}
+filtroPorPrecio(arrayStock)
+}
+
+//3check
+tercerCheckboxPrecio.addEventListener('change',tercerCheckFiltro,false)
+function tercerCheckFiltro (){
+const filtroPorPrecio = () => {
+  const precioSeleccionadoUsuario = tercerCheckboxPrecio.value
+  const arFiltradoPrecio3 = arrayStock.filter((vela)=>{
+    return vela.precio >= precioSeleccionadoUsuario
+  });
+    cargaProductos(arFiltradoPrecio3)
+}
+filtroPorPrecio(arrayStock)
+}
+
+//Intento de filtrar por precio con parametros
+function nombreGlobal (p1){
+  const filtroPorPrecio = () => {
+    const arFiltradoPrecio = arrayStock.filter((vela)=>{
+      return vela.precio >= p1.value
+    });
+      cargaProductos(arFiltradoPrecio)
+  }
+  filtroPorPrecio(arrayStock)
+  }
+
+
+//filtro por aroma
+let aroma1 = document.getElementById('aroma1')
+let aroma2 = document.getElementById('aroma2')
+let aroma3 = document.getElementById('aroma3')
+let aroma4 = document.getElementById('aroma4')
+
+const filtroPorAroma = (aroma) => {
+
+  const arFiltradoAroma = arrayStock.filter((vela)=>{
+    return vela.aroma.includes(aroma.value) == true
+  });
+  (arFiltradoAroma.length==0)?main.innerHTML = `Producto no encontrado`:cargaProductos(arFiltradoAroma)
+}
+
+aroma1.addEventListener('change',()=>filtroPorAroma(aroma1) ,false)
+aroma2.addEventListener('change',()=>filtroPorAroma(aroma2) ,false)
+aroma3.addEventListener('change',()=>filtroPorAroma(aroma3) ,false)
+aroma4.addEventListener('change',()=>filtroPorAroma(aroma4) ,false)
